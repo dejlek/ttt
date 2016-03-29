c3976c61890042c52f0203de9d177e693ce39e6f
Dejan Lekic <dejan@lekic.org>
2016-03-29 18:46:57 +0100

    Initial commit with the dummy Main class, and basic project skeleton.

---
+++ src/main/java/org/lekic/ttt/Main.java
@@ -0,0 +1,14 @@
+package org.lekic.ttt;
+
+/**
+ * This class contains the main entry point.
+ * @author dejan
+ */
+public class Main {
+    
+    
+    public static void main(String args[]) {
+        System.out.println("Initial code...");
+    }
+    
+} // Main class
